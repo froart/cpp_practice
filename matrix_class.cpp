fe0dd8e97096d7592771fa3915df145c25b7c09d
froart <artemiusxyz@gmail.com>
2023-08-07 17:30:06 +0200

    Implemented Matrix class and its member functions. Tested successfully.

---
+++ matrix_class.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <array>
+#include <memory>
+#include <algorithm>
+
+using namespace std;
+
+template<typename T>
+class Matrix {
+   private:
+       int rowNum_;
+       int colNum_;
+       vector<vector<T>> data_;
+   public:
+       Matrix() { // default constructor
+          this->rowNum_ = 0;
+          this->colNum_ = 0;
+       }; 
+       Matrix(int rowNum, int colNum) : rowNum_(rowNum), colNum_(colNum) { // parametrized constructor
+          this->data_.resize(this->rowNum_);
+          for(auto& row: this->data_)
+             row.resize(this->colNum_);
+          for(auto& row: this->data_)
+             for(auto& element: row)
+                element = (T)0; 
+       };
+       Matrix(initializer_list<initializer_list<T>> lst) { // parametrized constructor
+          this->rowNum_ = lst.size();
+          // find the longest nested initializer_list
+          int size = 0;
+          for(const auto& e: lst)
+             if(e.size() > size) size = e.size();
+          this->colNum_ = size;
+          // resize our matrix
+          this->data_.resize(this->rowNum_);
+          for(auto& row: this->data_)
+             row.resize(this->colNum_);
+          // map initializer_list values to out matrix
+          auto lst_row = lst.begin();
+          for(int j = 0; j < this->rowNum_; ++j) {
+             for(int i = 0; i < this->colNum_; ++i) {
+                this->data_[j][i] = (i > lst_row->size()) ? 0 : *(lst_row->begin() + i); // fill with zeroes for the incomplete initializer_lists
+             }
+             ++lst_row;
+          }
+       };
+       Matrix<T>& operator=(initializer_list<initializer_list<T>> lst) {
+          Matrix<T> result = lst;
+          *this = result; // copy
+          return *this;
+       }
+       Matrix(const Matrix<T>& other) { // copy constructor
+          this->colNum_ = other.colNum_;
+          this->rowNum_ = other.rowNum_;
+          this->data_ = other.data_;
+       };
+       Matrix<T>& operator=(const Matrix<T>& other) { // copy assign constructor
+          this->colNum_ = other.colNum_;
+          this->rowNum_ = other.rowNum_;
+          this->data_ = other.data_;
+          return *this;
+       }
+       void print() {
+          for(int i = 0; i < this->colNum_; ++i)
+             cout << "--";
+          cout << endl;
+          for(const auto& row: this->data_)
+             for(const auto& element: row)
+                cout << element << ((&element == &row.back()) ? '\n' : ' ');
+          for(int i = 0; i < this->colNum_; ++i)
+             cout << "--";
+          cout << endl;
+       };
+       Matrix<T> operator*(const Matrix<T>& other) {
+          if(this->colNum_ != other.rowNum_) // check matrices compatibility
+             throw invalid_argument("Matrices can't be multiplied due to their size incompatibility!");
+          Matrix<T> result = Matrix<T>(this->rowNum_, other.colNum_);
+          for(int j = 0; j < this->rowNum_; ++j)
+             for(int i = 0; i < other.colNum_; ++i)
+                for(int k = 0; k < this->colNum_; ++k)
+                   result.data_[j][i] += this->data_[j][k] * other.data_[k][i];
+          return result;
+       }; 
+};
+
+int main() {
+
+   Matrix<int> mat1 = {{1,1}, 
+                  {1,1,1},
+                  {1}};
+   Matrix<int> mat2 = {{1,1,1},
+                  {1,1,1},
+                  {1,1,1}};
+
+   cout << "mat1:" << endl;
+   mat1.print();
+   cout << "mat2:" << endl;
+   mat2.print();
+
+   Matrix<int> mat3; // default constructor
+   mat3 = {{1,2,3}, {4,5,6}, {7,8,9}}; // overload of the operator=(initializer_list<initializer_list<int>>)
+
+   Matrix<int> mat4 = mat1 * mat2;
+   cout << "Multiplication of mat1 and mat2: " << endl;
+   mat4.print();
+
+   Matrix<int> identity = {{1,0,0},
+                       {0,1,0},
+                       {0,0,1}}; 
+   Matrix<int> mat7 = identity * identity;
+   cout << "Multiplication of two identity matrices: " << endl;
+   mat7.print(); // must still be identity matrix
+   return 0;
+}
