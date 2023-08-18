1d738385ae8134bd3c8188dce8bc52da01b9834a
froart <artemiusxyz@gmail.com>
2023-08-18 04:58:05 +0200

    Reformatted the vulkan application structure. Vulkan instance created successfully.

--- vulkan/vulkan_tutorial.cpp
+++ vulkan/vulkan_tutorial.cpp
@@ -4,7 +4,6 @@
 #include <glm/mat4x4.hpp>
 #include <iostream>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL.h>
 #include <SDL2pp/SDL2pp.hh>
 
 using namespace std;
@@ -12,35 +11,76 @@ using namespace std;
 const int width = 640;
 const int height = 400;
 
-void updateFrameBuffer() {
-   return;
-}
+class TriangleApplication {
+   public:
+      void run() {
+         initWindow();
+         initVulkan();
+         mainLoop();
+         cleanUp();
+      }
 
-int main() {
-   SDL2pp::Window* window;
-   SDL2pp::Renderer* renderer;
-   try {
-      window = new SDL2pp::Window("My vulkan tutorial",
-                              SDL_WINDOWPOS_UNDEFINED, 
-                              SDL_WINDOWPOS_UNDEFINED,
-                              width, height,
-                              SDL_WINDOW_SHOWN);
-      renderer = new SDL2pp::Renderer(*window, -1, SDL_RENDERER_ACCELERATED);
-   } catch(SDL2pp::Exception& e) {
-      std::cerr << "Error in: " << e.GetSDLFunction() << std::endl;
-      std::cerr << "  Reason: " << e.GetSDLError() << std::endl;
-   }
+   private:
+      SDL2pp::Window* window_;
+      SDL2pp::Renderer* renderer_;
+      VkInstance instance_;
 
-   bool quit = false;
-   while(!quit) {
-      SDL_Event event;
-      SDL_PollEvent(&event);
-      if(event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE)
-         quit = true;
-      updateFrameBuffer();
-      renderer->Present();
-   }
-   delete renderer;
-   delete window;
-   return 0;
+      void initWindow() {
+         window_ = new SDL2pp::Window("My vulkan tutorial",
+                                      SDL_WINDOWPOS_UNDEFINED, 
+                                      SDL_WINDOWPOS_UNDEFINED,
+                                      width, height,
+                                      SDL_WINDOW_SHOWN);
+         renderer_ = new SDL2pp::Renderer(*window_, -1, SDL_RENDERER_ACCELERATED);
+      }
+
+      void createInstance() {
+         VkApplicationInfo appInfo{};
+         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+         appInfo.pApplicationName = "Hello triangle";
+         appInfo.applicationVersion = VK_MAKE_VERSION(1,0,0);
+         appInfo.pEngineName = "No Engine";
+         appInfo.engineVersion = VK_MAKE_VERSION(1,0,0);
+         appInfo.apiVersion = VK_API_VERSION_1_0;
+         VkInstanceCreateInfo createInfo{};
+         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+         createInfo.pApplicationInfo = &appInfo;
+         createInfo.enabledLayerCount = 0;
+         if(vkCreateInstance(&createInfo, nullptr, &instance_) != VK_SUCCESS)
+            throw std::runtime_error("failed to create instance!");
+      }
+
+      void initVulkan() {
+         createInstance();
+      }
+
+      void mainLoop() {
+         bool quit = false;
+         while(!quit) {
+            SDL_Event event;
+            SDL_PollEvent(&event);
+            if(event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE)
+               quit = true;
+            //updateFrameBuffer();
+            renderer_->Present();
+         }
+      }
+
+      void cleanUp() {
+         VkDestroyInstance(instance_, nullptr);
+         delete renderer_;
+         delete window_;
+      }
+};
+
+int main() try {
+   TriangleApplication app;
+// glm::mat4 matrix;
+// glm::vec4 vec;
+// auto test = matrix * vec;
+   app.run();
+   return EXIT_SUCCESS;
+} catch(const exception& e) {
+   cerr << e.what() << endl;
+   return EXIT_FAILURE;
 }
