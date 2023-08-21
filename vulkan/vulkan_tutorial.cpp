8c2e387b517f76a0c267dcbb9b5c91272a9238c7
froart <artemiusxyz@gmail.com>
2023-08-21 16:05:59 +0200

    Added the creation of a DebugUtilsMessenger

--- vulkan/vulkan_tutorial.cpp
+++ vulkan/vulkan_tutorial.cpp
@@ -53,7 +53,16 @@ class TriangleApplication {
          }
          return true;
       }
-
+    
+      static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+         vk::DebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+         vk::DebugUtilsMessageTypeFlagsEXT messageType,
+         const vk::DebugUtilsMessengerCallbackDataEXT* pCallbackData,
+         void* pUserData) {
+            cerr << "Validation layer: " << pCallbackData->pMessage << endl;
+            return VK_FALSE;
+      }
+    
       void initWindow() {
          window_ = new SDL2pp::Window("My vulkan tutorial",
                                       SDL_WINDOWPOS_UNDEFINED, 
@@ -63,25 +72,37 @@ class TriangleApplication {
          renderer_ = new SDL2pp::Renderer(*window_, -1, SDL_RENDERER_ACCELERATED);
       }
 
-      void createInstance() {
-         if(enableValidationLayers && !checkValidationLayerSupport())
+      void initVulkan() {
+         // CREATE INSTANCE
+         if(enableValidationLayers && !checkValidationLayerSupport()) // check if requested validation layers exist on the system
             throw runtime_error("Validation layers requested but not available");
-         vk::ApplicationInfo appInfo{ .pApplicationName = "Hello triangle",
+         vk::ApplicationInfo appInfo{ .pApplicationName = "Hello triangle", // specifying application information
                                       .applicationVersion = VK_MAKE_VERSION(1,0,0),
                                       .pEngineName = "No Engine",
                                       .engineVersion = VK_MAKE_VERSION(1,0,0),
                                       .apiVersion = VK_API_VERSION_1_0 };
-         vk::InstanceCreateInfo createInfo{ .pApplicationInfo = &appInfo };
+         vk::InstanceCreateInfo instanceCreateInfo { .pApplicationInfo = &appInfo }; // specifying instance information
          if(enableValidationLayers) {
-            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
-            createInfo.ppEnabledLayerNames = validationLayers.data();
-         } else createInfo.enabledLayerCount = 0;
-         if(vk::createInstance(&createInfo, nullptr, &instance_) != vk::Result::eSuccess)
+            instanceCreateInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+            instanceCreateInfo.ppEnabledLayerNames = validationLayers.data(); // add validation layers
+         } else instanceCreateInfo.enabledLayerCount = 0;
+         if(vk::createInstance(&instanceCreateInfo, nullptr, &instance_) != vk::Result::eSuccess) // creating instance
             throw std::runtime_error("failed to create instance!");
-      }
-
-      void initVulkan() {
-         createInstance();
+         // SETUP DEBUGGER
+         if(enableValidationLayers) {
+            vk::DebugUtilsMessageSeverityFlagsEXT severityFlags( vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose |
+                                                                   vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning |
+                                                                   vk::DebugUtilsMessageSeverityFlagBitsEXT::eError);
+            vk::DebugUtilsMessageTypeFlagsEXT messageTypeFlags( vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral |
+                                                                  vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation |
+                                                                  vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance);
+            vk::DebugUtilsMessengerEXT debugUtilsMessenger = instance_.createDebugUtilsMessengerEXT(
+                                                                           vk::DebugUtilsMessengerCreateInfoEXT {
+                                                                              .messageSeverity = severityFlags, 
+                                                                              .messageType = messageTypeFlags, 
+                                                                              .pfnUserCallback = nullptr /*debugCallback*/});
+                                                                              // FIXME debugCallback should be of a valid type
+         }
       }
 
       void mainLoop() {
