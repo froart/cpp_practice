ec325c1711ae4c55cecaccc838d8d3229c6724a6
froart <artemiusxyz@gmail.com>
2023-08-23 11:33:56 +0200

    Debugged the integration of SDL and Vulkan, plus minor design changes

--- vulkan/vulkan_tutorial.cpp
+++ vulkan/vulkan_tutorial.cpp
@@ -7,6 +7,7 @@
 #include <SDL2/SDL_vulkan.h>
 #include <SDL2pp/SDL2pp.hh>
 #include <cstring>
+#include <cassert>
 #include <vector>
 #include <algorithm>
 
@@ -14,6 +15,9 @@ using namespace std;
 
 const int width = 800;
 const int height = 600;
+
+// Enabling extensions
+vector<const char*> extensionNames = { VK_KHR_SURFACE_EXTENSION_NAME };
 
 // Enabling validation layers
 #ifndef NDEBUG
@@ -60,33 +64,55 @@ bool checkValidationLayerSupport() {
 int main(int argc, char** argv) try {
 
    // Creating SDL window
+   SDL2pp::SDL sdl(SDL_INIT_VIDEO);
    SDL2pp::Window window("My vulkan tutorial",
                          SDL_WINDOWPOS_UNDEFINED, 
                          SDL_WINDOWPOS_UNDEFINED,
                          width, height,
-                         SDL_WINDOW_SHOWN);
-   SDL2pp::Renderer renderer(window, -1, SDL_RENDERER_ACCELERATED);
+                         SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN); // Vulkan flag must be added in order to use SDL_Vulkan_CreateSurface()
 
-   // Initializing Vulkan
 #ifndef NDEBUG
    if(enableValidationLayers && !checkValidationLayerSupport()) // check if requested validation layers exist on the system
       throw runtime_error("Validation layers requested but not available");
 #endif
 
-   vk::ApplicationInfo appInfo ( "Hello triangle", VK_MAKE_VERSION(1,0,0), "No Engine", 1, VK_MAKE_VERSION(1,0,0) );
-   vk::InstanceCreateInfo instanceCreateInfo ( {}, &appInfo );
+   unsigned int count = 0;
 
-#ifndef NDEBUG
-   instanceCreateInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
-   instanceCreateInfo.ppEnabledLayerNames = validationLayers.data(); // add validation layers
+#ifndef NDEBUG // retrieve the number of extensions required to ntegrate SDL with Vulkan
+   assert(SDL_Vulkan_GetInstanceExtensions(window.Get(), &count, nullptr));
 #else
-// instanceCreateInfo.enabledLayerCount = 0;
-// instanceCreateInfo.pNext = nullptr;
+   SDL_Vulkan_GetInstanceExtensions(window.Get(), &count, nullptr);
 #endif
 
+   vector<const char*> SDL_extensions_required(count);
+
+#ifndef NDEBUG // retrieve the extensions required to integrate SDL with Vulkan
+   assert(SDL_Vulkan_GetInstanceExtensions(window.Get(), &count, SDL_extensions_required.data()));
+#else
+   SDL_Vulkan_GetInstanceExtensions(window.Get(), &count, SDL_extensions_required.data());
+#endif
+   for(int i = 0; i < count; ++i) // fill the Vulkan extension list with the required SDL extensions
+      extensionNames.push_back(SDL_extensions_required[i]);
+
+   // Creating Vulkan instance
+   vk::ApplicationInfo appInfo ( "Hello triangle", VK_MAKE_VERSION(1,0,0), "No Engine", 1, VK_MAKE_VERSION(1,0,0) );
+#ifndef NDEBUG
+   vk::InstanceCreateInfo instanceCreateInfo ( {}, &appInfo, validationLayers, extensionNames );
+#else
+   vk::InstanceCreateInfo instanceCreateInfo ( {}, &appInfo, {}, extensionNames );
+#endif
    vk::Instance instance = vk::createInstance ( instanceCreateInfo ); ;
 
+   // Binding SDL and Vulkan via Surface
+   vk::SurfaceKHR surface;
+   {
+      VkSurfaceKHR _surface;
+      assert(SDL_Vulkan_CreateSurface( window.Get(), static_cast<VkInstance>(instance), &_surface )); // bind Vulkan Surface to SDL Window
+      surface = vk::SurfaceKHR( _surface ); // move assignment
+   }
+
 #ifndef NDEBUG
+   // Create a customized DebugMessenger
 /* FIXME the function vkCreateDebugUtilsMessengerEXT throws a segfault
    vk::DebugUtilsMessageSeverityFlagsEXT severityFlags( vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose |
                                                         vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning |
@@ -98,41 +124,37 @@ int main(int argc, char** argv) try {
 */
 #endif
 
+   // Retrieve physical devices
    vk::PhysicalDevice physicalDevice = instance.enumeratePhysicalDevices().front(); // Choose the first found physical device
-   vk::PhysicalDeviceProperties physicalDeviceProperties = physicalDevice.getProperties(); // and retrieve its properties
 
+   // Query the physical device properties
+   vk::PhysicalDeviceProperties physicalDeviceProperties = physicalDevice.getProperties(); // and retrieve its properties
    cout << "Chosen Physical Device Information:" << endl; // print chosen physical device's properties
    cout << "Device Name: " << physicalDeviceProperties.deviceName << endl;
    cout << "API Version: " << VK_VERSION_MAJOR(physicalDeviceProperties.apiVersion) << "."
                            << VK_VERSION_MINOR(physicalDeviceProperties.apiVersion) << "."
                            << VK_VERSION_PATCH(physicalDeviceProperties.apiVersion) << endl;
-
-   vk::PhysicalDeviceFeatures physicalDeviceFeatures = physicalDevice.getFeatures(); // Retrieve physical device Features
-   
    if(physicalDeviceProperties.deviceType != vk::PhysicalDeviceType::eDiscreteGpu) // Warn if a chosen device is not a GPU
      cerr << "Chosen physical device is not a descrete GPU!" << endl;
 
+   // Query the physical device features
+   vk::PhysicalDeviceFeatures physicalDeviceFeatures = physicalDevice.getFeatures(); // Retrieve physical device Features
    if(!physicalDeviceFeatures.geometryShader) // Geometry Shader is a required feature
       throw runtime_error("Chosen physical device doesn't have a geometry shader!");
 
+   // Query the physical device queue families
    vector<vk::QueueFamilyProperties> queueFamilyProperties = physicalDevice.getQueueFamilyProperties();
    auto propertyIt = find_if(queueFamilyProperties.begin(), queueFamilyProperties.end(), // find eGraphics queue family
                              [](vk::QueueFamilyProperties const & qfp) { return qfp.queueFlags & vk::QueueFlagBits::eGraphics; } );
-
    size_t graphicsQueueFamilyIndex = distance(queueFamilyProperties.begin(), propertyIt);
+   size_t presentQueueFamilyIndex = physicalDevice.getSurfaceSupportKHR( static_cast<uint32_t>( graphicsQueueFamilyIndex ), surface ) // check if the index has a surface support
+                                    ? graphicsQueueFamilyIndex : queueFamilyProperties.size();
+   assert( presentQueueFamilyIndex == graphicsQueueFamilyIndex );
 
+   // Create a logical device
    float queuePriority = 0.0f;
    vk::DeviceQueueCreateInfo deviceQueueCreateInfo( vk::DeviceQueueCreateFlags(), static_cast<uint32_t>(graphicsQueueFamilyIndex), 1, &queuePriority );
    vk::Device device = physicalDevice.createDevice( vk::DeviceCreateInfo ( vk::DeviceCreateFlags(), deviceQueueCreateInfo ) );
-
-   vk::SurfaceKHR surface;    
-   {
-      VkSurfaceKHR _surface;
-      SDL_Vulkan_CreateSurface( window.Get(), static_cast<VkInstance>(instance), &_surface ); // bind Vulkan Surface to SDL Window
-      surface = vk::SurfaceKHR( _surface );
-   }
-   if(!&surface)
-      throw runtime_error("Surface cannot be created!");
 
    // Main Rendering Loop
    bool quit = false;
@@ -141,20 +163,24 @@ int main(int argc, char** argv) try {
       SDL_PollEvent(&event);
       if(event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE) // if ESC is hit
          quit = true;
-      renderer.Present(); // update Video Frame Buffer
    }
 
-   return EXIT_SUCCESS;
    // cleanup 
+   instance.destroySurfaceKHR( surface );
    device.destroy();
 #ifndef NDEBUG
-//   instance.destroyDebugUtilsMessengerEXT( debugUtilsMessenger );
+// instance.destroyDebugUtilsMessengerEXT( debugUtilsMessenger );
 #endif
    instance.destroy();
+   return EXIT_SUCCESS;
+
+} catch(SDL2pp::Exception& e) {
+   cerr << "Error in: " << e.GetSDLFunction() << endl;
+   cerr << "   Reason: " << e.GetSDLError() << endl;
+   return EXIT_FAILURE;
 
 } catch(const exception& e) {
-
    cerr << e.what() << endl;
-
    return EXIT_FAILURE;
-}
+
+} 
